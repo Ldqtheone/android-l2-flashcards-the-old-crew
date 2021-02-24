f372b090f7520f2407847345ca7bf39f7efae151
Corentinvls <corentin.vallois@gmail.com>
2021-02-24 16:57:52 +0100

    done

--- app/src/main/java/com/example/smash_card/HomeActivity.java
+++ app/src/main/java/com/example/smash_card/HomeActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -21,16 +22,23 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
         backgroundImageView.setBackgroundResource(R.drawable.ssbu_background);
 
         Button startQuizButton = findViewById(R.id.startQuizButton);
+        Button aboutButton = findViewById(R.id.aboutButton);
         startQuizButton.setOnClickListener(this);
+        aboutButton.setOnClickListener(this);
 
 
     }
 
     @Override
     public void onClick(View v) {
+        Intent intent;
         switch (v.getId()){
             case R.id.startQuizButton:
-                Intent intent = new Intent(HomeActivity.this, MainActivity.class);
+                 intent = new Intent(HomeActivity.this, MainActivity.class);
+                HomeActivity.this.startActivity(intent);
+                break;
+            case R.id.aboutButton:
+                 intent = new Intent(HomeActivity.this, About.class);
                 HomeActivity.this.startActivity(intent);
                 break;
         }
