e89dc730d4de95a3646ebd7750c767059c24a83a
Corentinvls <corentin.vallois@gmail.com>
2021-02-24 09:41:58 +0100

    done

--- app/src/main/java/com/example/smash_card/MainActivity.java
+++ app/src/main/java/com/example/smash_card/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.smash_card;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageView answeredImageView = findViewById(R.id.answeredImageView);
+        Picasso.get().load("https://static.wikia.nocookie.net/ssb/images/4/44/Mario_SSBU.png/revision/latest/scale-to-width-down/985?cb=20180612204958").into(answeredImageView);
+
     }
 }
