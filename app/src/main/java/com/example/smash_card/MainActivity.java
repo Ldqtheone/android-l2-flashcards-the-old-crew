a7b7efa407c108756955bbdd2f6f0cb360404761
Corentinvls <corentin.vallois@gmail.com>
2021-02-24 10:19:18 +0100

    in prog

--- app/src/main/java/com/example/smash_card/MainActivity.java
+++ app/src/main/java/com/example/smash_card/MainActivity.java
@@ -3,11 +3,16 @@ package com.example.smash_card;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import com.squareup.picasso.Picasso;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +20,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ImageView answeredImageView = findViewById(R.id.answeredImageView);
         Picasso.get().load("https://static.wikia.nocookie.net/ssb/images/4/44/Mario_SSBU.png/revision/latest/scale-to-width-down/985?cb=20180612204958").into(answeredImageView);
+        Button confirmButton = findViewById(R.id.confirmButton);
+        String goodAnswer = "Mario";
 
+        confirmButton.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        RadioGroup radioButtonGroup=findViewById(R.id.radioGroup);
+        RadioButton selected = findViewById(radioButtonGroup.getCheckedRadioButtonId());
+
+        switch (v.getId()){
+            case R.id.confirmButton:
+                if(selected.getText().toString().equals("Mario")){
+                    Log.i("answer","victory");
+                }else{
+                    Log.i("answer","tu pue ct mario");
+
+                }
+                break;
+        }
     }
 }
