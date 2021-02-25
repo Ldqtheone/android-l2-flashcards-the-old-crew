2885a5a35f609b5acbf162e4f5395e5548dce68d
Corentinvls <corentin.vallois@gmail.com>
2021-02-25 11:40:35 +0100

    done

--- app/src/main/java/com/example/smash_card/StatsEndQuiz.java
+++ app/src/main/java/com/example/smash_card/StatsEndQuiz.java
@@ -2,11 +2,21 @@ package com.example.smash_card;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.AssetFileDescriptor;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
-public class StatsEndQuiz extends AppCompatActivity {
+import org.json.JSONException;
+
+import java.io.IOException;
+
+public class StatsEndQuiz extends AppCompatActivity implements View.OnClickListener {
 
 
     @Override
@@ -17,7 +27,7 @@ public class StatsEndQuiz extends AppCompatActivity {
 
         int score = srcIntent.getIntExtra("score", 0);
         int question = srcIntent.getIntExtra("numberQuestion", 1);
-        int percent = Math.round(((float) score /(float)  question) * 100.0f);
+        int percent = Math.round(((float) score / (float) question) * 100.0f);
         TextView modeText = findViewById(R.id.modeText);
         modeText.setText(srcIntent.getStringExtra("mode"));
         TextView scoreText = findViewById(R.id.scoreText);
@@ -26,6 +36,18 @@ public class StatsEndQuiz extends AppCompatActivity {
         questionText.setText(question + "");
         TextView percentText = findViewById(R.id.scorePercent);
         percentText.setText(percent + "%");
+        Button buttonBackHome = findViewById(R.id.buttonBackHome);
+        buttonBackHome.setOnClickListener(this);
+    }
 
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+            case R.id.buttonBackHome:
+                Intent intent = new Intent(StatsEndQuiz.this, HomeActivity.class);
+                StatsEndQuiz.this.startActivity(intent);
+                break;
+        }
     }
 }
